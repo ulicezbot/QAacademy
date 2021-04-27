5ebbd8dca1118e80bde25ca44a967f14300c1848
unknown <you@example.com>
2021-04-27 16:59:33 -0300

    curso 2da clase

--- miprograma.py
+++ miprograma.py
@@ -1,27 +1,17 @@
-#programa de prueba
-#print("hola mundo")
+clientes=int(input("ingrese un numero de clientes: "))
+for i in range (clientes):
 
-#edad= 24
-edad= input("ingrese su edad: ")
-#print (edad)
-edad=int(edad)
-#if edad >= 18:
-#    print ("usted es mayor de edad")
-#    if edad ==65:
-#        print ("debe chequearse de corona")
-#else:
-#    print ("usted es menor de edad")
-if edad <0:
-    print ("usted no nacio")
-elif edad <18:
+  print(i+1)
+  edad= input("ingrese su edad: ")
+  edad=int(edad)
+  if edad <0:
+      print ("usted no nacio")
+  elif edad <18:
     print ("usted es menor de edad")
-elif edad <65:
+  elif edad <65:
     print ("usted es mayor de edad")
-elif edad< 120:
+  elif edad< 120:
     print ("usted es jubilado")
-else:
-    print ("de donde saliste flaco")
-a= "hola"
-b= " mundo"
-
-print (a+b)
+  else:
+    print ("isted esta muerto")
+    break
